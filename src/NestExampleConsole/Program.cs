8aa4b1fa056607f2c1baa0d8f5d1e0fdb85e840c
bramom <branimir.momcilovic@gmail.com>
2022-06-03 10:59:33 +0200

    New example added

--- src/NestExampleConsole/Program.cs
+++ src/NestExampleConsole/Program.cs
@@ -10,6 +10,13 @@ async Task<bool> AddDocument(ElasticClient client, Document document)
     return asyncIndexResponse.IsValid;
 }
 
+async Task<bool> AddZapisnik(ElasticClient client, Zapisnik zapisnik)
+{    
+    var asyncIndexResponse = await client.IndexDocumentAsync(zapisnik);
+    return asyncIndexResponse.IsValid;
+}
+
+
 void WriteDocuments (IReadOnlyCollection<Document>? documents)
 {
     if (documents != null)
@@ -28,11 +35,19 @@ var settings = new ConnectionSettings(pool)
     .CertificateFingerprint("5A:59:C9:5A:11:DB:EF:C8:CA:31:10:67:F1:74:B1:78:85:31:6E:4F:E5:11:EA:1B:4E:DD:AF:05:4E:9A:0B:EB")
     .BasicAuthentication("elastic", "kJkcftK2X8Skxp_Q6Ij_")
     .EnableApiVersioningHeader()
-    .DefaultIndex("documents");
+    .DefaultIndex("zapisnici_03");
 // .DefaultIndex("kibana_sample_data_ecommerce");
 
-var client = new ElasticClient(settings);
+//var client = new ElasticClient(settings);
 
+//var zapisnik = new Zapisnik
+//{
+//    Broj = "RZ_0006",
+//    KatastarskaOpstina = "KO_Batajnica",
+//    KatastarskaParcela = "123456"
+//};
+
+//var res = await AddZapisnik(client, zapisnik);
 
 //var document = new Document
 //{
@@ -45,43 +60,43 @@ var client = new ElasticClient(settings);
 //var addResult = await AddDocument(client, document);
 //Console.WriteLine($"Result {addResult}");
 
-var result = await client.Indices.GetAsync(new GetIndexRequest(Indices.All));
+//var result = await client.Indices.GetAsync(new GetIndexRequest(Indices.All));
 
-Console.WriteLine("Indices");
-Console.WriteLine(JsonSerializer.Serialize(result.Indices.Keys));
-
-
-var searchResponse = client.Search<Document>(s => s
-    .From(0)
-    .Size(10)
-    .Query(q => q
-         .Match(m => m
-            .Field(f => f.Type)
-            .Query("Ugovor")
-         )
-    )
-);
-
-var documents = searchResponse.Documents;
-
-WriteDocuments(documents);
+//Console.WriteLine("Indices");
+//Console.WriteLine(JsonSerializer.Serialize(result.Indices.Keys));
 
 
-Console.WriteLine("----");
-// https://localhost:9200/documents?pretty
-// https://localhost:9200/documents/_search?pretty
+//var searchResponse = client.Search<Document>(s => s
+//    .From(0)
+//    .Size(10)
+//    .Query(q => q
+//         .Match(m => m
+//            .Field(f => f.Type)
+//            .Query("Ugovor")
+//         )
+//    )
+//);
 
-var searchRN = client.Search<Document>(s => s
-    .From(0)
-    .Size(10)
-    .Query(q => q
-         .Match(m => m
-            .Field(f => f.No)
-            .Query("RN0001")
-         )
-    )
-);
+//var documents = searchResponse.Documents;
 
-var rns = searchRN.Documents;
+//WriteDocuments(documents);
 
-WriteDocuments(rns);
+
+//Console.WriteLine("----");
+//// https://localhost:9200/documents?pretty
+//// https://localhost:9200/documents/_search?pretty
+
+//var searchRN = client.Search<Document>(s => s
+//    .From(0)
+//    .Size(10)
+//    .Query(q => q
+//         .Match(m => m
+//            .Field(f => f.No)
+//            .Query("RN0001")
+//         )
+//    )
+//);
+
+//var rns = searchRN.Documents;
+
+//WriteDocuments(rns);
---
+++ src/NestExampleConsole/Zapisnik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestExampleConsole
+{
+    public class Zapisnik
+    {        
+        public string Broj { get; set; }        
+        public string KatastarskaOpstina { get; set; }
+        public string KatastarskaParcela { get; set; }
+    }
+}
