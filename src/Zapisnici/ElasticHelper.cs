cf38902815f6ffb105e8ecb8e6699a485f44e731
bramom <branimir.momcilovic@gmail.com>
2022-06-06 18:13:16 +0200

    ElasticSearch Lowlevel client example zapisnici

---
+++ src/Zapisnici/ElasticHelper.cs
@@ -0,0 +1,53 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+
+namespace Zapisnici
+{
+    public class ElasticHelper
+    {
+        public ElasticClient Client { get; set; }
+
+        public IElasticLowLevelClient LowLevelClient 
+        { 
+            get
+            {
+                return this.Client.LowLevel;
+            }
+        }
+        public ElasticHelper()
+        {
+            var pool = new SingleNodeConnectionPool(new Uri("https://localhost:9200"));
+
+            // openssl x509 -fingerprint -sha256 -in config/certs/http_ca.crt
+            var settings = new ConnectionSettings(pool)
+                .CertificateFingerprint("5A:59:C9:5A:11:DB:EF:C8:CA:31:10:67:F1:74:B1:78:85:31:6E:4F:E5:11:EA:1B:4E:DD:AF:05:4E:9A:0B:EB")
+                .BasicAuthentication("elastic", "kJkcftK2X8Skxp_Q6Ij_")
+                .EnableApiVersioningHeader()
+                .DefaultIndex("books");
+
+            this.Client = new ElasticClient(settings);            
+        }               
+
+        public async Task<bool> AddDocument(string index, string id, string json)
+        {
+            var indexResponse = await LowLevelClient.IndexAsync<StringResponse>(index, id, PostData.String(json));
+            return indexResponse.Success;
+        }
+
+        public async Task<VoidResponse> DeleteIndex(string index)
+        {
+            return await LowLevelClient.Indices.DeleteAsync<VoidResponse>(index);
+        }
+        
+        public async Task<StringResponse> Search(string index, string query)
+        {
+            return await LowLevelClient.SearchAsync<StringResponse>(index, query);
+        }
+    }
+}
---
+++ src/Zapisnici/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
+using Newtonsoft.Json;
+using Zapisnici;
+
+var helper = new ElasticHelper();
+var client = helper.LowLevelClient;
+
+Console.WriteLine("Delete indicies");
+var indicies = new List<string>() { "zapisnici_01", "zapisnici_02", "zapisnici_03" };
+indicies.ForEach(async index => await helper.DeleteIndex(index));
+
+// index a document from a JSON string, creating an index with auto-mapped properties
+var zapisnici = new List<Tuple<string, string, string>>()
+{
+    Tuple.Create("zapisnici_01", "1", "{ \"broj\": \"RZ_0001\", \"katastarskaOpstina\":\"KO_Batajnica\", \"katastarskaParcela\":\"12345\"}"),
+    Tuple.Create("zapisnici_01", "2", "{ \"broj\": \"RZ_0002\",\"katastarskaOpstina\":\"KO_Batajnica\",\"katastarskaParcela\":\"123456\"}"),
+    Tuple.Create("zapisnici_01", "3", "{ \"broj\": \"RZ_0003\",\"katastarskaOpstina\":\"KO_Batajnica\",\"katastarskaParcela\":\"1234567\"}"),
+    Tuple.Create("zapisnici_02", "4", "{ \"broj\": \"RZ_0004\",\"katastarskaOpstina\":\"KO_Batajnica\",\"katastarskaParcela\":\"23456\"}"),
+    Tuple.Create("zapisnici_02", "5", "{ \"broj\": \"RZ_0005\",\"katastarskaOpstina\":\"KO_Batajnica\",\"katastarskaParcela\":\"123456\"}"),
+    Tuple.Create("zapisnici_03", "6", "{ \"broj\": \"RZ_0006\",\"katastarskaOpstina\":\"KO_Batajnica\",\"katastarskaParcela\":\"123456\"}"),
+    Tuple.Create("zapisnici_03", "7", "{ \"broj\": \"RZ_0007\",\"katastarskaOpstina\":\"KO_Ugrinovci\",\"katastarskaParcela\":\"123456\"}")
+};
+
+Console.WriteLine("Adding data");
+foreach (var zapisnik in zapisnici)
+{
+    var result = await helper.AddDocument(zapisnik.Item1, zapisnik.Item2, zapisnik.Item3);
+    Console.Write($"{zapisnik.Item1}, {zapisnik.Item2} ");
+    Console.WriteLine(result ? "√" : "x");
+}
+Console.WriteLine();
+
+Console.WriteLine("Press <enter> to query data");
+Console.ReadLine();
+
+var zapisniciQuery = @"
+    {
+        ""query"": {
+            ""match"": {
+                ""katastarskaOpstina"": ""KO_Ugrinovci""
+            }
+        }
+    }";
+
+var searchResponse = await helper.Search("zapisnici*", zapisniciQuery);
+Console.WriteLine($"{searchResponse.Body}");
+
+if (searchResponse == null)
+{
+    Console.WriteLine("No documents found");
+} else
+{
+    dynamic stuff = JsonConvert.DeserializeObject(searchResponse.Body);
+
+    var i = 0;
+    foreach (var row in stuff.hits.hits)
+    {
+        Console.WriteLine(++i);
+        // Console.WriteLine($"{row._source["broj"]}");
+        Console.WriteLine($"{row}");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("That`s all folks");
